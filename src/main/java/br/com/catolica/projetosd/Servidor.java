01eb84b03613f77cf477b3b0a916ff7cbcbad471
Heitor <heitordiastavares3@gmail.com>
2024-04-10 23:23:24 -0300

    Adicionar tratamento de comandos para upload, download, deleção e listagem de arquiv

--- src/main/java/br/com/catolica/projetosd/Servidor.java
+++ src/main/java/br/com/catolica/projetosd/Servidor.java
@@ -36,5 +36,109 @@ public class Servidor {
             this.socket = socket;
         }
 
+        @Override
+        public void run() {
+            try {
+                out = new ObjectOutputStream(socket.getOutputStream());
+                in = new ObjectInputStream(socket.getInputStream());
+
+                String command;
+                while ((command = (String) in.readObject()) != null) {
+                    switch (command) {
+                        case "UPLOAD":
+                            handleUpload();
+                            break;
+                        case "DOWNLOAD":
+                            handleDownload();
+                            break;
+                        case "DELETE":
+                            handleDelete();
+                            break;
+                        case "LIST_FILES":
+                            sendFilesList();
+                            break;
+                        default:
+                            System.out.println("Comando inválido: " + command);
+                    }
+                }
+            } catch (IOException | ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+
+        private void handleUpload() throws IOException, ClassNotFoundException {
+            String fileName = (String) in.readObject();
+            File file = new File(UPLOAD_DIRECTORY, fileName);
+
+            try (FileOutputStream fos = new FileOutputStream(file)) {
+                byte[] buffer = new byte[4096];
+                int bytesRead;
+                while ((bytesRead = in.read(buffer)) != -1) {
+                    fos.write(buffer, 0, bytesRead);
+                }
+            }
+
+            System.out.println("Arquivo recebido: " + fileName);
+            sendFilesList();
+        }
+
+        private void handleDownload() throws IOException, ClassNotFoundException {
+            String fileName = (String) in.readObject();
+            File file = new File(UPLOAD_DIRECTORY, fileName);
+
+            boolean fileExists = file.exists() && file.isFile();
+            out.writeObject(fileExists);
+
+            if (fileExists) {
+                try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+                    List<String> fileContent = new ArrayList<>();
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        fileContent.add(line);
+                    }
+                    out.writeObject(fileContent);
+                }
+            }
+        }
+
+        private void handleDelete() throws IOException, ClassNotFoundException {
+            String fileName = (String) in.readObject();
+            File file = new File(UPLOAD_DIRECTORY, fileName);
+
+            boolean fileExists = file.exists() && file.isFile();
+            out.writeObject(fileExists);
+
+            if (fileExists) {
+                executor.execute(() -> {
+                    boolean fileDeleted = file.delete();
+                    if (fileDeleted) {
+                        System.out.println("Arquivo deletado: " + fileName);
+                        try {
+                            sendFilesList();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    } else {
+                        System.out.println("Falha ao deletar o arquivo: " + fileName);
+                    }
+                });
+            }
+        }
+
+        private void sendFilesList() throws IOException {
+            File directory = new File(UPLOAD_DIRECTORY);
+            File[] files = directory.listFiles();
+
+            List<String> filesList = new ArrayList<>();
+            if (files != null) {
+                for (File file : files) {
+                    if (file.isFile()) {
+                        filesList.add(file.getName());
+                    }
+                }
+            }
+
+            out.writeObject(filesList);
+        }
     }
 }
